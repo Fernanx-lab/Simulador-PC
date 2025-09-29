23d2e9bead222006acc39ef117ed06f911168ae7
Fernando-Felipe <nandomega11@gmail.com>
2025-09-29 20:47:03 -0300

    criando DTO Estado do simulador

---
+++ Utilidades/SimulationState.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.NetworkInformation;
+using System.Xml;
+
+namespace ProjetoSimuladorPC.Utilidades
+{
+    public class SimulationState
+    {
+        // Configurações fixas (YAML)
+        public Configuracoes Config { get; set; } = new Configuracoes();
+
+        // Controle do clock
+        public long CicloAtual { get; set; } = 0;
+
+        // Estados dos módulos
+        public CpuState Cpu { get; set; } = new CpuState();
+        public CacheState Cache { get; set; } = new CacheState();
+        public RamState Ram { get; set; } = new RamState();
+        public DmaState Dma { get; set; } = new DmaState();
+        public PicState Pic { get; set; } = new PicState();
+        public DevicesState Devices { get; set; } = new DevicesState();
+
+        // 🔹 Novo: estado do barramento separado
+        public BarramentoState Barramento { get; set; } = new BarramentoState();
+
+        // Métricas acumuladas
+        public MetricsState Metrics { get; set; } = new MetricsState();
+    }
+}
